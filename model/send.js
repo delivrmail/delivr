69e122cf16c251d67cc211335760d2426b00a8cb
hobbleabbas <shakeel@atlasfinance.org>
2022-01-12 11:56:56 -0500

    removed unneccesary comment and trailing comma

--- model/send.js
+++ model/send.js
@@ -2,27 +2,7 @@ const { exec } = require("child_process");
 
 const sendEmail = async (subject_line = "", body, to, from) => {
 	
-	const { error, stdout, stderr } = exec(`echo "${body}" | sudo mail -s "${subject_line}" ${to} -aFrom:${from}`, 
-        // if (error) {
-        //     console.log(`error: ${error.message}`);
-        //     return {
-        //         status: "error",
-        //         message: error
-        //     };
-        // }
-        // if (stderr) {
-        //     console.log(`stderr: ${stderr}`);
-        //     return {
-        //         status: "error",
-        //         message: stderr
-        //     };
-        // }
-
-        // return {
-        //     status: "error",
-        //     message: stdout
-        // };;
-    );
+	const { error, stdout, stderr } = exec(`echo "${body}" | sudo mail -s "${subject_line}" ${to} -aFrom:${from}`);
 
     if (error) {
         console.log(`error: ${error.message}`);
