7cfc2bbadcbf79309232a5d74b7256b54acf5cd8
hobbleabbas <74548347+hobbleabbas@users.noreply.github.com>
2022-01-14 16:49:54 -0500

    Update send.js

--- model/send.js
+++ model/send.js
@@ -4,18 +4,18 @@ const sendEmail = async (subject_line = "", body, to, from) => {
 	
 	const { error, stderr, stdout } = exec(`echo "${body}" | sudo mail -s "${subject_line}" ${to} -aFrom:${from}`);
 
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return {
-            status: "error",
-            message: error
-        };
-    }
+	if (error) {
+		console.log(`error: ${error.message}`);
+		return {
+		    status: "error",
+		    message: error
+		};
+	}
 
-    return {
-        status: "success",
-        message: stdout
-    };;
+	return {
+		status: "success",
+		message: stdout
+	};;
 }
 
 exports.sendEmail = sendEmail;
